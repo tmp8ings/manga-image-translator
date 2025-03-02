c4cf90b02c9c06e8d366ba49318cb6bbfbdf42a9
tmp8ings <k8888ings@gmail.com>
2025-03-03 01:11:32 +0900

    rendering log 추가

--- manga_translator/rendering/__init__.py
+++ manga_translator/rendering/__init__.py
@@ -137,6 +137,7 @@ def render(
     norm_v = np.linalg.norm(middle_pts[:, 2] - middle_pts[:, 0], axis=1)
     r_orig = np.mean(norm_h / norm_v)
 
+    logger.debug(f'norm_h {norm_h}, norm_v {norm_v}, r_orig {r_orig}, region {region}')
     if True or region.horizontal:
         temp_box = text_render.put_text_horizontal(
             region.font_size,
